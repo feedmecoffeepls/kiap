886dcf06ddd1e9092146c1d68a5256acda4c4a0c
Isaac <isaac.quek@gmail.com>
2024-07-12 23:22:42 +0800

    Remove banner select due to lack of time

--- components/items/imageDialog.tsx
+++ components/items/imageDialog.tsx
@@ -3,18 +3,14 @@
 import React, { useState } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTrigger, DialogTitle, DialogDescription } from '../ui/dialog';
 import { Button } from '../ui/button';
-import { SelectItem, SelectItemImages } from '@/db/schema';
+import { SelectItem } from '@/db/schema';
 import { UploadDropzone } from "@/lib/uploadthing";
-import { createImages, setBanner } from '@/actions/itemImagesActions';
+import { createImages } from '@/actions/itemImagesActions';
 import { toast } from '../ui/use-toast';
 
-interface PurchaseDialogSchema extends SelectItem {
-    refetch: () => void;
-    images: SelectItemImages[];
-}
 
 type PurchaseDialogProps = {
-    item: PurchaseDialogSchema;
+    item: SelectItem;
     refetch: () => void;
 };
 
@@ -22,20 +18,6 @@ const ImageDialog: React.FC<PurchaseDialogProps> = ({ item, refetch }) => {
 
     const [error, setError] = useState<string | null>(null);
     const [open, setOpen] = useState(false)
-
-    const noBanner = item.images.every(image => !image.is_banner);
-
-    const handleSetBanner = async (imageId: number) => {
-        const success = await setBanner(item.id, imageId)
-        if (success) {
-            toast({
-                title: "Banner set",
-                description: "We have set the banner",
-            })
-            await refetch()
-        }
-    }
-
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild><Button>Manage Images</Button></DialogTrigger>
@@ -59,17 +41,6 @@ const ImageDialog: React.FC<PurchaseDialogProps> = ({ item, refetch }) => {
                         alert(`ERROR! ${error.message}`);
                     }}
                 />
-                <div>
-                    <h6>{item.title}'s images</h6>
-                    <div className="flex flex-wrap">
-                        {item.images.map((image, index) => (
-                            <div key={image.id} className="m-2 border rounded" onClick={() => handleSetBanner(image.id)}>
-                                <img src={image.blob_url} alt={item.title} className="h-20 w-20 object-cover" />
-                                {noBanner && index == 0 ? <p className="text-center font-bold">Banner</p> : image.is_banner ? <p className="text-center font-bold">Banner</p> : ""}
-                            </div>
-                        ))}
-                    </div>
-                </div>
             </DialogContent>
         </Dialog>
     );
